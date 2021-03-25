07a85b37c7e8d6cf602df944bef0aa0dfb00d1c8
Issam57 <issam.boucheraki@gmail.com>
2021-03-26 00:19:29 +0100

    Premier commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                gris: '#191919',
+                rose: '#FFCDC1',
+                bleu: '#CBE9FF',
+                vert: '#E0FCB5'
+            }
+        }
+    }
+}
